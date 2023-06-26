b5a713874b1f084db6d56dbea53b11715f70a05a
6semlab <133591894+6semlab@users.noreply.github.com>
2023-06-26 11:15:17 +0530

    Add files via upload

---
+++ threeaddress.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+char op[2], arg1[5], arg2[5], res[5];
+int main() {
+  FILE *fp1, *fp2;
+  fp1 = fopen("input.txt", "r");
+  fp2 = fopen("output.txt", "w");
+  while(!feof(fp1)) {
+    fscanf(fp1, "%s%s%s%s", res, arg1, op, arg2);
+    if(strcmp(op, "+") == 0) {
+      fprintf(fp2, "\nMOV R0, %s", arg1);
+      fprintf(fp2, "\nADD R0, %s", arg2);
+      fprintf(fp2, "\nMOV %s, R0", res);
+    }
+
+    if(strcmp(op, "-") == 0) {
+      fprintf(fp2, "\nMOV R0, %s", arg1);
+      fprintf(fp2, "\nSUB R0, %s", arg2);
+      fprintf(fp2, "\nMOV %s, R0", res);
+    }
+
+    if(strcmp(op, "*") == 0) {
+      fprintf(fp2, "\nMOV R0, %s", arg1);
+      fprintf(fp2, "\nMUL R0, %s", arg2);
+      fprintf(fp2, "\nMOV %s, R0", res);
+    }
+
+    if(strcmp(op, "/") == 0) {
+      fprintf(fp2, "\nMOV R0, %s", arg1);
+      fprintf(fp2, "\nDIV R0, %s", arg2);
+      fprintf(fp2, "\nMOV %s, R0", res);
+    }
+
+    if(strcmp(op, "=") == 0) {
+      fprintf(fp2, "\nMOV R0, %s", arg1);
+      fprintf(fp2, "\nMOV %s, R0", res);
+    }  
+  }
+  fclose(fp1);
+  fclose(fp2);
+}
